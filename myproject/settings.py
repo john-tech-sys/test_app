ae655e83be525f8351937ea68a8f8046b156510c
John Revival Aomu <70672905+john-tech-sys@users.noreply.github.com>
2024-08-05 16:43:12 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ol4qqevgicmfbkz^98y&tju1n(kzat-dm$=84o!r9ty3m-kvvm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
